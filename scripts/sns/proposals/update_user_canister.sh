2f04f944e772c7b7bda1f20b5382709cbdd372c3
dragon1227 <dragon1227@gmail.com>
2023-06-20 06:24:09 +0200

    fix: updated sns proposal script

--- scripts/sns/proposals/update_user_canister.sh
+++ scripts/sns/proposals/update_user_canister.sh
@@ -1,3 +1,4 @@
+# ./update_user_canister.sh <Developer_Neuron_ID> <Username> <User_Canister_ID> <User_Age> <User_Name> <User_Email>
 # eg './update_user_canister.sh 5ee58180b48d54ca91f394a42e4c036d43a82e1095e4ff5275e0cb14c2140abc administrator bw4dl-smaaa-aaaaa-qaacq-cai 22 James dragon1227@outlook.com'
 
 # Set current directory to the directory this script is in
@@ -18,7 +19,7 @@ USER_CANISTER_ID=$3
 USER_AGE=$4
 USER_NAME=$5
 USER_EMAIL=$6
-IC_URL="https://192.168.146.128:3000"
+IC_URL="http://localhost:8000"
 PAYLOAD=$(didc encode '("'$USER_CANISTER_ID'", record { user=record {age='$USER_AGE':nat64; name="'$USER_NAME'":text; email="'$USER_EMAIL'":text;};})' --format blob)
 
 printf 'DEVELOPER_NEURON_ID: %s\n' "$DEVELOPER_NEURON_ID"
@@ -30,8 +31,10 @@ printf 'USER_NAME: %s\n' "$USER_NAME"
 printf 'USER_EMAIL: %s\n' "$USER_EMAIL"
 
 # Make the proposal using quill
-quill sns --canister-ids-file ../../../sns_canister_ids.json --pem-file $PEM_FILE make-proposal --proposal "(record { title=\"Register SNS user update generic function.\"; url=\"https://example.com/\"; summary=\"This proposal registers SNS user update generic functions.\"; action=opt variant {AddGenericNervousSystemFunction = record {id=1000:nat64; name=\"MyGenericFunctions\"; description=null; function_type=opt variant {GenericNervousSystemFunction=record{validator_canister_id=opt principal\"$CID\"; target_canister_id=opt principal\"$CID\"; validator_method_name=opt\"sns_update_user_canister_validate\"; target_method_name=opt\"sns_update_user_canister\"}}}}})" $DEVELOPER_NEURON_ID > register-generic-functions.json
-quill send register-generic-functions.json
+quill sns --canister-ids-file ../../../sns_canister_ids.json --pem-file $PEM_FILE make-proposal --proposal "(record { title=\"Register SNS user update generic function.\"; url=\"$IC_URL\"; summary=\"This proposal registers SNS user update generic functions.\"; action=opt variant {AddGenericNervousSystemFunction = record {id=1000:nat64; name=\"MyGenericFunctions\"; description=null; function_type=opt variant {GenericNervousSystemFunction=record{validator_canister_id=opt principal\"$CID\"; target_canister_id=opt principal\"$CID\"; validator_method_name=opt\"sns_update_user_canister_validate\"; target_method_name=opt\"sns_update_user_canister\"}}}}})" $DEVELOPER_NEURON_ID > register-generic-functions.json
+quill send --yes --insecure-local-dev-mode register-generic-functions.json
+rm -f register-generic-functions.json
 
-quill sns --canister-ids-file ../../../sns_canister_ids.json --pem-file $PEM_FILE make-proposal --proposal "(record { title=\"Execute SNS update user function.\"; summary=\"This proposal executes SNS update user function.\"; action=opt variant {ExecuteGenericNervousSystemFunction = record {function_id=1000:nat64; payload=$PAYLOAD}}})" $DEVELOPER_NEURON_ID > execute-update-user-function.json
-quill send execute-update-user-function.json
+quill sns --canister-ids-file ../../../sns_canister_ids.json --pem-file $PEM_FILE make-proposal --proposal "(record { title=\"Execute SNS update user function.\"; url=\"$IC_URL\"; summary=\"This proposal executes SNS update user function.\"; action=opt variant {ExecuteGenericNervousSystemFunction = record {function_id=1000:nat64; payload=$PAYLOAD}}})" $DEVELOPER_NEURON_ID > execute-update-user-function.json
+quill send --yes --insecure-local-dev-mode execute-update-user-function.json
+rm -f execute-update-user-function.json
--- user-canister-wasm-service/app.js
+++ user-canister-wasm-service/app.js
@@ -18,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger((tokens, req, res) => {
+  return `${tokens.method(req, res)} ${tokens.url(req, res)} ${tokens.status(req, res)} - ${tokens['response-time'](req, res)} ms`;
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
